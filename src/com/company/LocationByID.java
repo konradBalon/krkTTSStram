5100a62c99e95b5dd897bfbd05a0830189992d96
Konrad <konrad.balon@gmail.com>
2019-07-09 15:20:42 +0200

    move to rest

--- src/com/company/LocationByID.java
+++ src/com/company/LocationByID.java
@@ -1,28 +1,26 @@
 package com.company;
 
 
-import com.sun.tools.javac.Main;
-
 import java.util.List;
 
 public class LocationByID extends MainParameters {
 
-    public String currentLocation(String id, List<Tram> t) throws Exception {
-
-        for (Tram tram : t) {
+    public String currentLocation(String id) {
+        MainParameters mainParameters = new MainParameters();
+        for (Tram tram : mainParameters.getAllTramProperties()) {
             if (tram.getId().equals(id)) {
-          //      System.out.println("coordinate: " + tram.getLatitude() + "   " + tram.getLongitude());
+                //      System.out.println("coordinate: " + tram.getLatitude() + "   " + tram.getLongitude());
 
                 return Double.toString(tram.getLongitude() + tram.getLatitude());
 
             }
         }
-       return "helo";
+        return "o";
     }
 
-    public void printCurrentLocation(String id, List<Tram> t) {
-
-        for (Tram tram : t) {
+    public void printCurrentLocation(String id) {
+        MainParameters parameters = new MainParameters();
+        for (Tram tram : parameters.getAllTramProperties()) {
             if (tram.getId().equals(id))
                 System.out.println("coordinates: " + tram.getLatitude() + "   " + tram.getLongitude());
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,22 +11,15 @@ public class Main {
         LocationByID location = new LocationByID();
 
 
-//        for (int i = 0; i < 100; i++) {
-//            MainParameters params = new MainParameters();
-//            location.printCurrentLocation("-1188950297150681465", params.getAllTramProperties());
-//            i++;
-//            TimeUnit.SECONDS.sleep(5);
-//        }
+        for (int i = 0; i < 100; i++) {
 
-
-        while (true) {
-            String id = "-1188950297150681465";
-            String loc = null;
-            MainParameters param1 = new MainParameters();
-            loc = (location.currentLocation(id, param1.getAllTramProperties()));
-
-
+            location.printCurrentLocation("-1188950297150681465");
+            i++;
+            TimeUnit.SECONDS.sleep(5);
         }
+
+
+
     }
 }
 
